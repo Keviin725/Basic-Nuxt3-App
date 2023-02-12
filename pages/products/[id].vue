97ead19dcb29bbc20a6e5b1912e68b4a53c31864
Kevin Machava <kevinmachava20@gmail.com>
2023-02-12 23:48:23 +0200

    novas paginas

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quidem iste soluta expedita molestias voluptates beatae voluptatem odit fugit eum illum, blanditiis repellat aperiam. Nobis consequatur obcaecati ea laudantium numquam?</p>
+    </div>
+</template>
+
+<script>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
