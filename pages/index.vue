22bb5b772dade365d866978ec361610dacd32636
Kevin Machava <kevinmachava20@gmail.com>
2023-02-12 23:35:27 +0200

    updates

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>HomePage</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ut voluptas similique rem deserunt tempora labore cumque id. Ullam quas blanditiis neque dolores vero eos. Laudantium nulla explicabo iusto nesciunt?</p>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
